34e597d3d3bd05ed31dfe63c5baf199d00784f9f
Nada Fadali <nada.fadali@gmail.com>
2016-03-30 17:05:39 +0200

    projectupdate model, adapter and view controller

---
+++ Robustivity/Robustivity/Adapters/ProjectUpdateAdapter.swift
@@ -0,0 +1,9 @@
+//
+//  ProjectUpdateAdapter.swift
+//  Robustivity
+//
+//  Created by Nada Fadali on 3/30/16.
+//  Copyright © 2016 BumbleBee. All rights reserved.
+//
+
+import Foundation
---
+++ Robustivity/Robustivity/Models/ProjectUpdateModel.swift
@@ -0,0 +1,16 @@
+//
+//  ProjectUpdateModel.swift
+//  Robustivity
+//
+//  Created by Nada Fadali on 3/30/16.
+//  Copyright © 2016 BumbleBee. All rights reserved.
+//
+
+import UIKit
+
+class ProjectUpdateModel: BaseModel {
+    var userName:String?
+    var userAvatar:String?
+    var updateTime:String?
+    var updateContnet:String?
+}
