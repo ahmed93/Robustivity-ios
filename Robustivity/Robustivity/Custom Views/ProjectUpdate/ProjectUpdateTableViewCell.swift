380fba8f3b0483b4bda00315a565080887fc2ff0
Nada Fadali <nada.fadali@gmail.com>
2016-03-30 16:41:37 +0200

    project update tableviewcell

---
+++ Robustivity/Robustivity/Custom Views/ProjectUpdate/ProjectUpdateTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProjectUpdateTableViewCell.swift
+//  Robustivity
+//
+//  Created by Nada Fadali on 3/30/16.
+//  Copyright © 2016 BumbleBee. All rights reserved.
+//
+
+import UIKit
+
+class ProjectUpdateTableViewCell: BaseTableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
