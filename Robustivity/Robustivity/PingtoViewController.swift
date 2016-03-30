49ce84d39ee23e09fdc9f6bffc35e67fe61665d1
Alaa Maher <almgohar@gmail.com>
2016-03-30 23:59:50 +0200

    Intial Setup of View Controllers

---
+++ Robustivity/Robustivity/PingtoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PingtoViewController.swift
+//  Robustivity
+//
+//  Created by Almgohar on 3/30/16.
+//  Copyright © 2016 BumbleBee. All rights reserved.
+//
+
+import UIKit
+
+class PingtoViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Ping To";
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
