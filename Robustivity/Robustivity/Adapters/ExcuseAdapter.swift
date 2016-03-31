5161ce99c9f66c25f657f4b449d23f1cc001505a
Abdelrahman Saad <abdelrahmansaad@Macbooks-MacBook-Pro.local>
2016-03-31 19:11:46 +0200

    test push

---
+++ Robustivity/Robustivity/Adapters/ExcuseAdapter.swift
@@ -0,0 +1,42 @@
+//
+//  ExcuseAdapter.swift
+//  Robustivity
+//
+//  Created by Abdelrahman Saad  on 3/30/16.
+//  Copyright © 2016 BumbleBee. All rights reserved.
+//
+
+import UIKit
+
+class ExcuseAdapter: BaseTableAdapter {
+    
+    
+    override init(viewController: UIViewController, tableView: UITableView, registerCellWithNib name: String, withIdentifier identifier: String) {
+        super.init(viewController: viewController, tableView: tableView, registerCellWithNib: name, withIdentifier: identifier)
+        
+        // any extra stuff to be done
+    }
+    
+    
+    func fetchItems() {
+        if tableItems == nil {
+            tableItems = ListModel()
+        }
+        tableItems.addObject("I am too tired!")
+        tableItems.addObject("I wanna Sleep!")
+        tableItems.addObject("I have personal stuff!")
+        tableView.reloadData()
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    }
+    
+    override func configure(cell: UITableViewCell, indexPath: NSIndexPath) {
+        let _cell = cell as? BaseTableViewCell
+        
+        _cell?.label.text = tableItems.objectAtIndex(indexPath.row) as? String
+    }
+    
+    
+}
+
