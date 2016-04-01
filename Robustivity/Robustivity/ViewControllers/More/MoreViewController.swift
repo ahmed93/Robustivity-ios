60ad4231a7067a6cb51d2c1784fd4a38aeae7ea3
SalmaAmrAhmed <salmaumr@gmail.com>
2016-04-01 14:24:58 +0200

    add multiple cells

--- Robustivity/Robustivity/ViewControllers/More/MoreViewController.swift
+++ Robustivity/Robustivity/ViewControllers/More/MoreViewController.swift
@@ -11,12 +11,16 @@ import UIKit
 class MoreViewController: BaseViewController {
 
 
+    @IBOutlet weak var logOut: UIButton!
+    
     @IBOutlet weak var jobTitle: UILabel!
+
 
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var avatar: UIImageView!
     @IBOutlet weak var profileMore: UIView!
     @IBOutlet weak var tableView: UITableView!
+    
     var adapter:OptionsTableAdapter!
 
     required init?(coder aDecoder: NSCoder) {
@@ -29,21 +33,15 @@ class MoreViewController: BaseViewController {
         super.viewDidLoad()
         self.title = "More";
         self.navigationItem.title = "More";
-
         setProfileMore();
     }
     
     func setProfileMore(){
         self.profileMore.backgroundColor = Theme.getColor(Color.lightGray);
-      //  self.avatar.image = UIImage (named:"/Assets.xcassets/islam.png");
-
         self.avatar.layer.cornerRadius = self.avatar.frame.size.width / 2;
         self.avatar.clipsToBounds = true;
-        
-        adapter = OptionsTableAdapter(viewController: self, tableView: tableView, registerCellWithNib:"MoreTableViewCell", withIdentifier: "MoreCell");
-    //    adapter.
-        
-    
+        let dict:NSDictionary = ["MoreTableViewCell":"MoreCell", "BaseTableViewCell":"cell" ];
+         adapter = OptionsTableAdapter(viewController: self, tableView: tableView, registerMultipleNibsAndIdenfifers: dict);
     }
     
 
