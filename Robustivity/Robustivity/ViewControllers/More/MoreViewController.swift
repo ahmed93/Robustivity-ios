2d9c110dd13c98990c9aed2ce91b0ea465c15389
mayarelmohr <mayar.elmohr@gmail.com>
2016-04-02 02:49:50 +0200

    fixed styling, reduced margin in table, fitt width into screen, added shadow

--- Robustivity/Robustivity/ViewControllers/More/MoreViewController.swift
+++ Robustivity/Robustivity/ViewControllers/More/MoreViewController.swift
@@ -40,9 +40,31 @@ class MoreViewController: BaseViewController {
     }
     
     func setProfileMore(){
+        /** Setting backround color **/
         self.profileMore.backgroundColor = Theme.getColor(Color.lightGray);
+        
+        /** Creating a shadow after the UIView **/
+
+        self.profileMore.clipsToBounds = false;
+        self.profileMore.layer.shadowColor = Theme.getColor(Color.black).CGColor;
+        self.profileMore.layer.shadowOffset = CGSizeMake(0,3);
+        self.profileMore.layer.shadowOpacity = 0.5;
+
+        
+        /** a Rounded avatar **/
         self.avatar.layer.cornerRadius = self.avatar.frame.size.width / 2;
         self.avatar.clipsToBounds = true;
+       
+        /** Styling labels **/
+        Theme.style_2(self.checkedInLabel);
+        Theme.style_2(self.checkInTimeLabel);
+        Theme.style_13(self.name);
+        Theme.style_1(self.jobTitle);
+       
+        /** To hide margin in tableView **/
+        self.tableView.contentInset = UIEdgeInsetsMake(-36, 0, 0, 0);
+
+        /** Add cells to the tableView **/
         let dict:NSDictionary = ["MoreTableViewCell":"MoreCell", "BaseTableViewCell":"cell" ];
          adapter = OptionsTableAdapter(viewController: self, tableView: tableView, registerMultipleNibsAndIdenfifers: dict);
     }
