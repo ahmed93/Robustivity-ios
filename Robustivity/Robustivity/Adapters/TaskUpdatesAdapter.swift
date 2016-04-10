65ef4ff7241fb9646cd6a6a30c4b3d56d3c2b549
Mansour Said <Mansour@Mansours-MacBook-Pro.local>
2016-04-11 00:34:04 +0200

    Fixing bottom updates bar

--- Robustivity/Robustivity/Adapters/TaskUpdatesAdapter.swift
+++ Robustivity/Robustivity/Adapters/TaskUpdatesAdapter.swift
@@ -19,16 +19,8 @@ class TaskUpdatesAdapter: BaseTableAdapter{
         super.init(viewController: viewController, tableView: tableView, registerCellWithNib: name, withIdentifier: identifier)
         
         // any extra stuff to be done
-        
-        
     }
-    
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        
-    }
-    
-    
-    
+  
     override func configure(cell: UITableViewCell, indexPath: NSIndexPath) {
         let _cell = cell as? BaseTableViewCell
         
@@ -48,30 +40,7 @@ class TaskUpdatesAdapter: BaseTableAdapter{
         return cell
     }
     
-    func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        let footerView = UIView(frame:CGRectMake(0,0,320,40))
-        footerView.backgroundColor = Theme.lightGrayColor()
-        let commentButton = UIButton(type: .Custom)
-        commentButton.setTitle("Comment", forState: .Normal)
-        Theme.style_11(commentButton.titleLabel!)
-        commentButton.setTitleColor(Theme.blueColor(), forState: .Normal)
-        commentButton.backgroundColor = Theme.whiteColor()
-        commentButton.addTarget(self, action: nil, forControlEvents: .TouchUpInside)
-        commentButton.frame = CGRectMake(UIScreen.mainScreen().bounds.width - 100, 0, 100, 51)
-        footerView.addSubview(commentButton)
-        
-        let textfield = UITextField(frame: CGRect(x: 0, y: 0, width: UIScreen.mainScreen().bounds.width - commentButton.frame.width, height: 51))
-        textfield.placeholder = "   Write Comment Here..."
-        textfield.backgroundColor = Theme.whiteColor()
-        footerView.addSubview(textfield)
-        
-        return footerView
-    }
-
-    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return 51
-    }
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return 6
     }
 }
--- Robustivity/Robustivity/ViewControllers/Task View/TaskViewController.swift
+++ Robustivity/Robustivity/ViewControllers/Task View/TaskViewController.swift
@@ -16,10 +16,12 @@ import UIKit
 
 class TaskViewController: BaseViewController {
 
+    @IBOutlet weak var bottomTableConstraint: NSLayoutConstraint!
     
     @IBOutlet weak var viewUpper: UIView!
     var infoAdapter:TaskInfoAdapter!
     var updatesAdapter:TaskUpdatesAdapter!
+    var footerView:UIView!
     @IBOutlet weak var table: UITableView!
     var customSC:UISegmentedControl!
     
@@ -32,6 +34,27 @@ class TaskViewController: BaseViewController {
         infoAdapter = TaskInfoAdapter(viewController: self, tableView: table, registerMultipleNibsAndIdenfifers: dic)
         self.table.backgroundColor = Theme.lightGrayColor()
         self.automaticallyAdjustsScrollViewInsets = false
+        
+        footerView = UIView(frame:CGRectMake(0,0,320,40))
+        footerView.backgroundColor = Theme.lightGrayColor()
+        let commentButton = UIButton(type: .Custom)
+        commentButton.setTitle("Comment", forState: .Normal)
+        Theme.style_11(commentButton.titleLabel!)
+        commentButton.setTitleColor(Theme.blueColor(), forState: .Normal)
+        commentButton.backgroundColor = Theme.whiteColor()
+        commentButton.addTarget(self, action: nil, forControlEvents: .TouchUpInside)
+        commentButton.frame = CGRectMake(UIScreen.mainScreen().bounds.width - 100, 0, 100, 51)
+        footerView.addSubview(commentButton)
+        
+        let textfield = UITextField(frame: CGRect(x: 0, y: 0, width: UIScreen.mainScreen().bounds.width - commentButton.frame.width, height: 51))
+        textfield.placeholder = "   Write Comment Here..."
+        textfield.backgroundColor = Theme.whiteColor()
+        footerView.addSubview(textfield)
+        footerView.frame.origin.y = UIScreen.mainScreen().bounds.height - 100
+        footerView.hidden = true
+        self.view.addSubview(footerView)
+        //bottomTableConstraint.constant = 52
+        
     }
     override func loadView() {
         super.loadView()
@@ -72,12 +95,18 @@ class TaskViewController: BaseViewController {
                 ,   "DescriptionTableViewCell" : "descriptionCell",
             "TaskInfoToggledTableViewCell":"toggleCell"]
             infoAdapter = TaskInfoAdapter(viewController: self, tableView: table, registerMultipleNibsAndIdenfifers: dic)
+            footerView.hidden = true
+            bottomTableConstraint.constant = 8
+            self.view.layoutIfNeeded()
             table.reloadData()
             break
         case 1:
             table.separatorStyle = .None
             updatesAdapter = TaskUpdatesAdapter(viewController: self, tableView: table, registerCellWithNib: "CommentTableViewCell", withIdentifier: "commentCell")
             table.reloadData()
+            footerView.hidden = false
+            bottomTableConstraint.constant = 100
+            self.view.layoutIfNeeded()
             break
         default:
             break
