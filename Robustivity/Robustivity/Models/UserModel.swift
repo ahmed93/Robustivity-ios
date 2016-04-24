4589a4cfc3634335fea01cf82c7db6c92d9a336c
narimanhesham <nariman.hesham93@gmail.com>
2016-04-24 15:37:00 +0200

    Adding user model

--- Robustivity/Robustivity/Models/UserModel.swift
+++ Robustivity/Robustivity/Models/UserModel.swift
@@ -7,13 +7,51 @@
 //
 
 import UIKit
+import Foundation
+import ObjectMapper
+import RealmSwift
 
-class UserModel: BaseModel {
-    var avatar:NSURL?
-    var name:String?
+class UserModel: Object, Mappable {
+    dynamic var userId = 0
+    dynamic var userEmail = ""
+    dynamic var userFirstName = ""
+    dynamic var userLastName = ""
+    dynamic var userMobileNumber = ""
+    dynamic var userAddress = ""
+    dynamic var userTitle = ""
+    dynamic var userContactPersonName = ""
+    dynamic var userContactPersonPhone = ""
+    dynamic var userContactPersonRelation = ""
+    dynamic var userContactPersonAddress = ""
+    dynamic var userProfilePictureURL = ""
+    dynamic var userProfilePictureProfileURL = ""
+    dynamic var userProfilePictureSquareURL = ""
+    dynamic var userProfilePictureIconURL = ""
+    dynamic var userProfilePictureNotificationURL = ""
+    dynamic var userCity = ""
     
-    init(avatar:NSURL, name:String) {
-        self.avatar = avatar
-        self.name = name
+    
+    required convenience init?(_ map: Map) {
+        self.init()
+    }
+    
+    func mapping(map: Map) {
+        userId                              <- map["id"]
+        userEmail                           <- map["email"]
+        userFirstName                       <- map["first_name"]
+        userLastName                        <- map["last_name"]
+        userMobileNumber                    <- map["mobile_number"]
+        userAddress                         <- map["address"]
+        userTitle                           <- map["title"]
+        userContactPersonName               <- map["contact_person_name"]
+        userContactPersonPhone              <- map["contact_person_phone"]
+        userContactPersonRelation           <- map["contact_person_relation"]
+        userContactPersonAddress            <- map["contact_person_address"]
+        userProfilePictureURL               <- map["profile_picture"]["url"]
+        userProfilePictureProfileURL        <- map["profile_picture"]["profile"]["url"]
+        userProfilePictureSquareURL         <- map["profile_picture"]["square"]["url"]
+        userProfilePictureIconURL           <- map["profile_picture"]["icon"]["url"]
+        userProfilePictureNotificationURL   <- map["profile_picture"]["notifications"]["url"]
+        userCity                            <- map["city"]
     }
 }
