8aa254d9e1557c1f82150fa6dd6bc7ad01dbad1e
khaled <khaled.elhossiny10@gmail.com>
2016-04-27 18:11:01 +0200

    done with creating tasks and todods

--- Robustivity/Robustivity/ViewControllers/Planner/Create Task/Choose Assignee/ChooseAssigneeViewController.swift
+++ Robustivity/Robustivity/ViewControllers/Planner/Create Task/Choose Assignee/ChooseAssigneeViewController.swift
@@ -14,6 +14,7 @@ class ChooseAssigneeViewController: BaseViewController {
     var adapter:UserAdapter!
 
     @IBOutlet weak var tableView: UITableView!
+    var project_id:Int!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Assign To";
