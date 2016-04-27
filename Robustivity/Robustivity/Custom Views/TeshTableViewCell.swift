94d932ea45c0f4efa29e24ad9b2676591d47341b
jihan2032 <jihan2032@gmail.com>
2016-04-27 19:38:35 +0200

    Test

---
+++ Robustivity/Robustivity/Custom Views/TeshTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TeshTableViewCell.swift
+//  Robustivity
+//
+//  Created by Jihan Adel on 4/27/16.
+//  Copyright © 2016 BumbleBee. All rights reserved.
+//
+
+import UIKit
+
+class TeshTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
