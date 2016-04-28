fce13adb5143015d069e17344e836ad27cea1727
Alaa Maher <almgohar@gmail.com>
2016-04-28 12:39:33 +0200

    my projects backend

--- Robustivity/Robustivity/API/APIRoutes.swift
+++ Robustivity/Robustivity/API/APIRoutes.swift
@@ -16,6 +16,7 @@ class APIRoutes {
     static let USER_SHOW = "users/"
     static let USER_PROFILE = USER_SHOW + "profile/"
     
+    static let MY_PROJECTS = "projects/my_projects/";
     
    
     
