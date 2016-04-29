022bb1c36356688987ed39017ca857bcb1913563
Mimic <Abanoub.aziz92@gmail.com>
2016-04-29 02:37:25 +0200

    add backend

--- Robustivity/Robustivity/ViewControllers/More/Projects/ProjectInfoViewController.swift
+++ Robustivity/Robustivity/ViewControllers/More/Projects/ProjectInfoViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ProjectInfoViewController: BaseViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var footerView: UIView!
     @IBOutlet weak var projectInfoTableView: UITableView!
     @IBOutlet weak var projectTasksProgress: UIProgressView!
     @IBOutlet weak var projectCashProgress: UIProgressView!
@@ -19,20 +21,14 @@ class ProjectInfoViewController: BaseViewController {
     @IBOutlet weak var projectCustomerSatisfactionValueLabel: UILabel!
     @IBOutlet weak var projectStatusLabel: UILabel!
     @IBOutlet weak var projectDateLabel: UILabel!
+    var projectSponserNumber:String = ""
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
-    
     var adapter: ProjectInfoAdapter!
-    
-//    required init?(coder aDecoder: NSCoder) {
-//        super.init(coder: aDecoder)
-//        NSBundle.mainBundle().loadNibNamed("ProjectInfoViewController", owner: self, options: nil)
-//    }
     
     override func loadView() {
         super.loadView()
@@ -40,38 +36,72 @@ class ProjectInfoViewController: BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        adapter = ProjectInfoAdapter(viewController: self, tableView: projectInfoTableView, registerCellWithNib:"ProjectMemberCell", withIdentifier: "projectMembers")
         self.title = "Robustivity Project";
-        
+        customizeUI()
+    }
+    
+    func callSponser(sender: UIButton!){
+        let phoneNumber = "tel://" + self.projectSponserNumber
+        let url:NSURL = NSURL(string: phoneNumber)!
+        UIApplication.sharedApplication().openURL(url)
+    }
+    
+    func setData(project:Project){
+        projectStatusLabel?.text = project.projectStatus
+        projectDateLabel?.text = project.projectEndDate
+        let tasksValue = Float(project.projectTasksDone) / Float(project.projectNumberOfTasks)
+        projectTasksDoneLabel?.text = "\(String(project.projectTasksDone))/\(String(project.projectNumberOfTasks))"
+        projectTasksProgress?.progress = tasksValue
+        let cashValue = Float(project.projectActualCost) / Float(project.projectPlannedCost)
+        projectCashValueLabel?.text = "\(String(project.projectActualCost))/\(String(project.projectPlannedCost))"
+        projectCashProgress?.progress = cashValue
+        let customerSatisification = project.projectCustomerStatisfaction
+        projectCustomerSatisfactionValueLabel?.text = customerSatisification
+        if(customerSatisification == "high"){
+            projectCustomerSatisfactionProgress?.progress = 1.0
+        }else if (customerSatisification == "medium"){
+            projectCustomerSatisfactionProgress?.progress = 0.5
+        }else{
+            projectCustomerSatisfactionProgress?.progress = 0.0
+        }
+    }
+    
+    func customizeUI(){
         projectTasksProgress?.tintColor = Theme.greenColor();
         projectTasksProgress?.trackTintColor = Theme.whiteColor();
-        projectTasksProgress?.setProgress(0.3, animated: true)
         projectTasksProgress?.layer.borderWidth = 0.7
         projectTasksProgress?.layer.borderColor = Theme.greenColor().CGColor
-        projectTasksDoneLabel?.text = "23/30"
         projectTasksDoneLabel?.textColor = Theme.greenColor()
         
         projectCashProgress?.tintColor = Theme.blueColor();
         projectCashProgress?.trackTintColor = Theme.whiteColor();
-        projectCashProgress?.setProgress(0.5, animated: true)
         projectCashProgress?.layer.borderWidth = 0.7
         projectCashProgress?.layer.borderColor = Theme.blueColor().CGColor
-        projectCashValueLabel?.text = "$100"
         projectCashValueLabel?.textColor = Theme.blueColor()
         
         projectCustomerSatisfactionProgress?.tintColor = Theme.purpleColor();
         projectCustomerSatisfactionProgress?.trackTintColor = Theme.whiteColor();
-        projectCustomerSatisfactionProgress?.setProgress(1.0, animated: true)
         projectCustomerSatisfactionProgress?.layer.borderWidth = 0.7
         projectCustomerSatisfactionProgress?.layer.borderColor = Theme.purpleColor().CGColor
-        projectCustomerSatisfactionValueLabel?.text = "100%"
         projectCustomerSatisfactionValueLabel?.textColor = Theme.purpleColor()
         
-        projectStatusLabel?.text = "In Progress"
-        projectStatusLabel?.textColor = Theme.greenColor()
-        projectDateLabel?.text = "Oct 15, 2015"
-        projectDateLabel?.textColor = Theme.redColor()
+//        projectStatusLabel?.textColor = Theme.greenColor()
+//        projectDateLabel?.textColor = Theme.redColor()
+//        
+//        self.tableView.addSubview(projectTasksDoneLabel)
+//        self.tableView.addSubview(projectTasksProgress)
+//        
+//        self.tableView.addSubview(projectCashValueLabel)
+//        self.tableView.addSubview(projectCashProgress)
+//
+//        self.tableView.addSubview(projectCustomerSatisfactionValueLabel)
+//        self.tableView.addSubview(projectTasksProgress)
+
+//        self.tableView.tableFooterView = self.footerView
         
-        adapter = ProjectInfoAdapter(viewController: self, tableView: projectInfoTableView, registerCellWithNib:"ProjectMemberCell", withIdentifier: "projectMembers")
+    
+
     }
     
     /*
--- Robustivity/Robustivity/ViewControllers/More/Projects/ProjectSegmentedControlViewController.swift
+++ Robustivity/Robustivity/ViewControllers/More/Projects/ProjectSegmentedControlViewController.swift
@@ -12,36 +12,39 @@ import ObjectMapper
 
 class ProjectSegmentedControlViewController: BaseViewController {
     
+    
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var viewsSegmentedControl: UISegmentedControl!
     
     var projectInfoViewController:ProjectInfoViewController?
     var projectTeamViewController:ProjectTeamViewController?
     var projectUpdateViewController:ProjectUpdateViewController?
+    var project:Project?
+    var sponserNumber = ""
     
     var data = NSMutableArray()
     
-    var project_id = 1
-
+    var project_id = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         self.title = "Robustivity Project"
         self.navigationItem.title = "Robustivity Project"
         
         requestProject()
-
+        
         // load intial subview
         viewsSegmentedControl.selectedSegmentIndex = 0
         
-    
-
+        
+        
         // style segmented control
         self.viewsSegmentedControl.backgroundColor = Theme.redColor();
         self.viewsSegmentedControl.tintColor = Theme.whiteColor();
         
-
+        
     }
     
     func requestProject() {
@@ -52,7 +55,29 @@ class ProjectSegmentedControlViewController: BaseViewController {
                 let projectTeam = Mapper<User>().mapArray(response["members"])
                 self.data.addObjectsFromArray(projectTeam!)
                 self.showSubView(0)
-
+                                
+                // Project Show
+                //map the jason object to the model and save them
+                let project = Mapper<Project>().map(response["project"])
+                project?.projectNumberOfTasks = response["number_of_tasks"] as! Int
+                project?.projectTasksDone = response["number_of_completed_tasks"] as! Int
+                self.projectInfoViewController?.adapter.saveNewProject(project!)
+                self.projectInfoViewController?.setData(project!)
+                let projectManagerName:String = (project?.projectManagerName)!
+                let projectAccountManagerName:String = (project?.projectAccountManagerName)!
+                let projectManagerAvatar:String = (project?.projectManagerAvatar)!
+                let projectAccountManagerAvatar:String = (project?.projectAccountManagerAvatar)!
+                let projectSponserName:String = (project?.projectSponserName)!
+                let billable:Bool = (project?.projectIsBillable)!
+                let billAmount:String = String((project?.projectQuotation)!)
+                let projectSponserNumber:String = (project?.projectSponserPhone)!
+                self.projectInfoViewController?.projectSponserNumber = projectSponserNumber
+                self.sponserNumber = (project?.projectSponserPhone)!
+                self.projectInfoViewController?.adapter.tableItems.addObject(["name": projectManagerName, "role" : "Project Manager", "type": "1", "avatar": projectManagerAvatar])
+                self.projectInfoViewController?.adapter.tableItems.addObject(["name": projectAccountManagerName, "role" : "Account Manager", "type": "1", "avatar": projectAccountManagerAvatar])
+                self.projectInfoViewController?.adapter.tableItems.addObject(["name": projectSponserName, "role" : "Spoc", "telephoneNumber": projectSponserNumber ,"type": "2"])
+                self.projectInfoViewController?.adapter.tableItems.addObject(["billable": billable, "billAmount" : billAmount , "type": "3"])
+                self.projectInfoViewController?.adapter.tableView.reloadData()
             }
         })
     }
@@ -99,28 +124,28 @@ class ProjectSegmentedControlViewController: BaseViewController {
             projectTeamViewController!.view.frame = self.containerView.frame
             self.containerView.addSubview(projectTeamViewController!.view)
             
-            default:
+        default:
             break
         }
         
-//        
-//        self.containerView.subviews[viewIndex].hidden = false
-//        for var i = 0; i < 3; i++ {
-//            if (i != viewIndex) {
-//                self.containerView.subviews[i].hidden = true
-//            }
-//        }
+        //
+        //        self.containerView.subviews[viewIndex].hidden = false
+        //        for var i = 0; i < 3; i++ {
+        //            if (i != viewIndex) {
+        //                self.containerView.subviews[i].hidden = true
+        //            }
+        //        }
     }
     
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
