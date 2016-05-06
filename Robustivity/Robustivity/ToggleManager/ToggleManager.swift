038640797f18b61a99a37be4996a6d0452df1ffa
AhmedElassuty <ahmed.elassuty214@gmail.com>
2016-05-07 01:44:18 +0200

    Remove commented lines

--- Robustivity/Robustivity/ToggleManager/ToggleManager.swift
+++ Robustivity/Robustivity/ToggleManager/ToggleManager.swift
@@ -24,7 +24,8 @@ import RealmSwift
         return Static.instance!
     }
 
-    var timer = NSTimer();
+    private var timer = NSTimer();
+
     var startDate = NSDate();
     var currentTimeInterval = NSTimeInterval();
     var pausedTimeInterval = NSTimeInterval();
@@ -200,91 +201,6 @@ import RealmSwift
             tasks.setValue("paused", forKey: "taskStatus")
         }
     }
-    
-    // old
-//    func togglePauseAction(onSuccess: (()->())?) {
-//        guard let toggledTask = toggledTask else { return }
-//        
-//        let url = formatURL(APIRoutes.TASK_TIMELOG_PAUSE, stringToReplace: "task_id", with: toggledTask.taskId)
-//        let requestParams : [String: AnyObject] = ["task": toggledTask] //these params is not needed
-//        
-//        self.timer.invalidate(); //stop timer
-//        API.put(url, parameters: requestParams, callback: { (success, response) in
-//            if(success) {
-//                self.pausedTimeInterval = self.currentTimeInterval
-//                try! self.realm.write {
-//                    toggledTask.taskStatus = "paused"
-//                }
-//                
-////                self.delegate?.toggleManager?(self, didPauseTimer: self.toggledTime, forTask: toggledTask)
-//                self.pauseTimer()
-//
-//                onSuccess?()
-//            } else {
-//                self.startTimer()
-//            }
-//        })
-//        
-//    }
-    
-//    func toggleResumeAction(){
-//        toggleResumeAction(toggledTask!) { }
-//    }
-    
-//    func toggleResumeAction(newTask: TaskModel, onSuccess:()->()) {
-//        let url = formatURL(APIRoutes.TASK_TIMELOG_RESUME, stringToReplace: "task_id", with: toggledTask!.taskId)
-//        let requestParams : [String: AnyObject] = ["task": newTask] //these params is not needed
-//        
-//        self.timer.invalidate()
-//        API.post(url, parameters: requestParams, callback: { (success, response) in
-//            if(success) {
-//                // ***probably wrong log time for old task
-//                let playingTasks = self.realm.objects(TaskModel).filter("taskStatus = 'in_progress'")
-//                for playingTask in playingTasks {
-//                    try! self.realm.write {
-//                        playingTask.taskStatus = "paused"
-//                    }
-//                }
-//                
-//                let task = Mapper<TaskModel>().map(response["task"])
-//
-//                task?.updateTask()
-//                self.toggledTask = task!
-//                self.startDate = self.toggledTask!.taskUpdatedAt!
-//                let interval:NSTimeInterval = Double(self.toggledTask!.taskDuration)
-//                self.pausedTimeInterval = interval
-//                
-//                self.startTimer()
-//
-//                onSuccess()
-//                
-//            } else {
-//                self.startTimer()
-//            }
-//        })
-//        
-//    }
-    
-//    func toggleStopAction(onSuccess: ()->()) {
-//        guard let toggledTask = toggledTask else { return }
-//        
-//        let url = formatURL(APIRoutes.TASK_TIMELOG_STOP, stringToReplace: "task_id", with: toggledTask.taskId)
-//        let requestParams : [String: AnyObject] = ["task": toggledTask] //these params is not needed
-//
-//        self.timer.invalidate();
-//        API.put(url, parameters: requestParams, callback: { (success, response) in
-//            if(success) {
-//                try! self.realm.write {
-//                    toggledTask.taskStatus = "paused"
-//                }
-//                
-//                self.stopTimer()
-//                onSuccess()
-//            } else {
-//                self.startTimer()
-//            }
-//        })
-//    }
     
     /*
     ** updateToggledTime
